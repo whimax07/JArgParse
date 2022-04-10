be1c657d4fc28c4588a6450ef5915f65131e5104
whimax07 <59751270+whimax07@users.noreply.github.com>
2022-04-10 21:32:03 +0100

    Added some more tests for Long Arg and Mixed.

--- tests/MixedInputTest.java
+++ tests/MixedInputTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MixedInputTest {
 
     ArgsParser.ProgrammeDetails makeProgrammeDetails() {
@@ -9,10 +11,29 @@ public class MixedInputTest {
 
 
     @Test
-    void pareArgs_passOnEmptyCommandLine() {
+    void pass_on_empty_command_line() {
         ArgsParser argsParser = new ArgsParser(makeProgrammeDetails(), EnumArgOptions.class);
         String[] input = new String[] {};
+        assertDoesNotThrow(() -> argsParser.pareArgs(input));
+    }
+
+    @Test
+    void pass_return_null_for_not_used_arg() {
+        ArgsParser argsParser = new ArgsParser(makeProgrammeDetails(), EnumArgOptions.class);
+        String[] input = new String[] {"--Set-Text=Hi"};
         argsParser.pareArgs(input);
+        assertNull(argsParser.getArgument(EnumArgOptions.BACKGROUND));
+    }
+
+    @Test
+    void pass_enum_and_arg_string_return_the_same() {
+        ArgsParser argsParser = new ArgsParser(makeProgrammeDetails(), EnumArgOptions.class);
+        String[] input = new String[] {"--Set-Text=Hi"};
+        argsParser.pareArgs(input);
+        assertSame(
+                argsParser.getArgument(EnumArgOptions.TEXT),
+                argsParser.getArgument(EnumArgOptions.TEXT.get().getLongKey())
+        );
     }
 
 }
