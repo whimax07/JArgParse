3b1fa13d2ed6268f8f69721203ec1b496a03f008
whimax07 <59751270+whimax07@users.noreply.github.com>
2022-04-19 22:39:06 +0100

    Long key tests are done for the moment.

--- tests/LongKeyTests.java
+++ tests/LongKeyTests.java
@@ -46,15 +46,31 @@ public class LongKeyTests {
         assertTrue(argsParser.isPassed(EnumArgOptions.TEXT));
         assertTrue(argsParser.isPassed("Set-Text"));
         assertTrue(argsParser.isLongPassed("Set-Text"));
+        assertTrue(argsParser.isShortPassed('t'));
         assertNotNull(argsParser.getArgument(EnumArgOptions.TEXT));
         assertNotNull(argsParser.getArgument("Set-Text"));
+        assertNotNull(argsParser.getArgument("t"));
     }
 
     /**
      * See {@link MixedInputTests#pass_multiple_same_input_arguments_allowed()}.
      */
     @Test
+    void pass_get_passed_values() { }
+
+    @Test
     void pass_confirm_key_value_pair_not_passed() {
+        ArgsParser argsParser = new ArgsParser(makeProgrammeDetails(), EnumArgOptions.class);
+        String[] input = new String[] {"--Set-Text=Hi"};
+        assertDoesNotThrow(() -> argsParser.pareArgs(input));
+
+        assertFalse(argsParser.isPassed(EnumArgOptions.RESET));
+        assertFalse(argsParser.isPassed("Use-Defaults"));
+        assertFalse(argsParser.isLongPassed("Use-Defaults"));
+        assertFalse(argsParser.isShortPassed('r'));
+        assertNull(argsParser.getArgument(EnumArgOptions.RESET));
+        assertNull(argsParser.getArgument("Use-Defaults"));
+        assertNull(argsParser.getArgument("r"));
     }
 
 
@@ -63,31 +79,51 @@ public class LongKeyTests {
     //    Key.
     // ===============================
     @Test
-    void fail_key_passed_key_value() {
-        ArgsParser argsParser = new ArgsParser(makeProgrammeDetails(), EnumArgOptions.class);
-        // Need to pass set text so it errors. If you don't it just looks like a list arg. Probably why they are bad.
-        String[] input = new String[] {"-r", "reset_all", "--Set-Text=(0,0,0)"};
-        assertThrows(ArgsParser.ParseArgumentException.class, () -> argsParser.pareArgs(input));
-    }
-
-    @Test
     void pass_pass_key() {
-        fail();
+        ArgsParser argsParser = new ArgsParser(makeProgrammeDetails(), EnumArgOptions.class);
+        String[] input = new String[] {"--Use-Defaults"};
+        assertDoesNotThrow(() -> argsParser.pareArgs(input));
     }
 
     @Test
     void pass_confirm_key_passed() {
-        fail();
+        ArgsParser argsParser = new ArgsParser(makeProgrammeDetails(), EnumArgOptions.class);
+        String[] input = new String[] {"--Use-Defaults"};
+        argsParser.pareArgs(input);
+
+        assertTrue(argsParser.isPassed(EnumArgOptions.RESET));
+        assertTrue(argsParser.isPassed("Use-Defaults"));
+        assertTrue(argsParser.isLongPassed("Use-Defaults"));
+        assertTrue(argsParser.isShortPassed('r'));
+        assertNotNull(argsParser.getArgument(EnumArgOptions.RESET));
+        assertNotNull(argsParser.getArgument("Use-Defaults"));
+        assertNotNull(argsParser.getArgument("r"));
     }
 
     @Test
     void pass_get_key_passed() {
-        fail();
+        ArgsParser argsParser = new ArgsParser(makeProgrammeDetails(), EnumArgOptions.class);
+        String[] input = new String[] {"--Use-Defaults"};
+        argsParser.pareArgs(input);
+
+        String passValue = argsParser.getArgument(EnumArgOptions.RESET).getValue();
+        assertNotNull(passValue);
+        assertTrue(passValue.isEmpty());
     }
 
     @Test
     void pass_confirm_key_not_passed() {
-        fail();
+        ArgsParser argsParser = new ArgsParser(makeProgrammeDetails(), EnumArgOptions.class);
+        String[] input = new String[] {"--Use-Defaults"};
+        argsParser.pareArgs(input);
+
+        assertFalse(argsParser.isPassed(EnumArgOptions.TEXT));
+        assertFalse(argsParser.isPassed("Set-Text"));
+        assertFalse(argsParser.isLongPassed("Set-Text"));
+        assertFalse(argsParser.isShortPassed('t'));
+        assertNull(argsParser.getArgument(EnumArgOptions.TEXT));
+        assertNull(argsParser.getArgument("Set-Text"));
+        assertNull(argsParser.getArgument("t"));
     }
 
 
@@ -99,6 +135,14 @@ public class LongKeyTests {
     void fail_pass_the_same_key_more_than_once_without_allowing() {
         ArgsParser argsParser = new ArgsParser(makeProgrammeDetails(), EnumArgOptions.class);
         String[] input = new String[] {"--Set-Text=Hi", "--Set-Text=Yo"};
+        assertThrows(ArgsParser.ParseArgumentException.class, () -> argsParser.pareArgs(input));
+    }
+
+    @Test
+    void fail_key_passed_key_value() {
+        ArgsParser argsParser = new ArgsParser(makeProgrammeDetails(), EnumArgOptions.class);
+        // Need to pass set text so it errors. If you don't it just looks like a list arg. Probably why they are bad.
+        String[] input = new String[] {"--Use-Defaults", "reset_all", "--Set-Text=(0,0,0)"};
         assertThrows(ArgsParser.ParseArgumentException.class, () -> argsParser.pareArgs(input));
     }
 
